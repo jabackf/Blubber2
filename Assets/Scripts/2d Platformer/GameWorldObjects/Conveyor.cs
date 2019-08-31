30ed5d4d31b235570b450592bb7fc67816b61501
jabackf <jabackf@yahoo.com>
2019-08-31 05:54:09 -0700

    Conveyors finally seem to be working right!

--- Assets/Scripts/2d Platformer/GameWorldObjects/Conveyor.cs
+++ Assets/Scripts/2d Platformer/GameWorldObjects/Conveyor.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class Conveyor : MonoBehaviour
 {
-    public float speed = -100f;
+    public float speed = -2f;
     public bool printDebugInfo = false;
+    public bool pauseFor5 = false;
+    public bool printTopList = false; 
+
+    private Animator animator;
+    private string animSpeedMultiplier = "SpeedMultiplier";
+    private bool paused = false;
+    private float pauseTime = 4f;
+    private float speedPrevious;
 
     public class objEntry
     {
@@ -14,10 +22,11 @@ public class Conveyor : MonoBehaviour
         public Rigidbody2D rb;
         public CharacterController2D characterController;
 
-        public objEntry(GameObject go)
+        public objEntry(GameObject go, Rigidbody2D rb = null)
         {
             this.gameObject = go;
-            this.rb = this.gameObject.GetComponent<Rigidbody2D>();
+            if (rb == null) this.rb = this.gameObject.GetComponent<Rigidbody2D>();
+            else this.rb = rb;
             this.characterController = this.gameObject.GetComponent<CharacterController2D>();
             if (this.characterController != null) this.characterController.setIsOnConveyor(true);
         }
@@ -29,12 +38,63 @@ public class Conveyor : MonoBehaviour
 
     List<objEntry> objects = new List<objEntry>();
 
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat(animSpeedMultiplier, speed);
+
+    }
+
+    public void changeSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+        animator.SetFloat(animSpeedMultiplier, speed);
+    }
+
+    //The following method will pause the conveyor for the specified amount of time
+    void pause (float time=5f)
+    {
+        if (printDebugInfo) Debug.Log("Conveyor Paused!");
+        if (!paused) speedPrevious = speed;
+        changeSpeed(0);
+        paused = true;
+        pauseTime = time;
+    }
+
+    void unPause()
+    {
+        if (printDebugInfo) Debug.Log("Conveyor UnPaused!");
+        paused = false;
+        pauseFor5 = false;
+        changeSpeed(speedPrevious);
+    }
+
+    void OnValidate()
+    {
+        changeSpeed(speed);
+
+        if (pauseFor5 == true)
+        {
+            pause();
+        }
+
+        if (printTopList)
+        {
+            Debug.Log(gameObject.name + " TopCount: " + objects.Count());
+            foreach (objEntry obj in objects)
+            {
+                Debug.Log(gameObject.name+" TopItem: "+obj.gameObject.name);
+            }
+            printTopList = false;
+        }
+    }
+
     void FixedUpdate()
     {
         foreach(objEntry obj in objects)
         {
             //obj.rb.AddForce(new Vector2(speed, 0));
-            if (printDebugInfo) Debug.Log(obj.gameObject.name + " < Applied Motion: POS:" + (new Vector2(obj.rb.position.x, obj.rb.position.y)) + "Spd: "+ new Vector2(speed, 0) + "Vel: "+ obj.rb.velocity);
+            //if (printDebugInfo) Debug.Log(obj.gameObject.name + " < Applied Motion: POS:" + (new Vector2(obj.rb.position.x, obj.rb.position.y)) + "Spd: "+ new Vector2(speed, 0) + "Vel: "+ obj.rb.velocity);
             //if (obj.rb.velocity.y<3)
             obj.rb.MovePosition( new Vector2(obj.rb.position.x+speed* Time.fixedDeltaTime, obj.rb.position.y) + (obj.rb.velocity*Time.fixedDeltaTime) );
             //obj.rb.velocity = new Vector2( (obj.rb.velocity.x+speed) * Time.fixedDeltaTime, obj.rb.velocity.y);
@@ -46,6 +106,18 @@ public class Conveyor : MonoBehaviour
 
         }
     }
+    
+    void Update()
+    {
+        if (paused)
+        {
+            pauseTime -= Time.deltaTime;
+            if (pauseTime <= 0)
+            {
+                unPause();
+            }
+        }
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -54,14 +126,15 @@ public class Conveyor : MonoBehaviour
         {
             if (other.gameObject.transform.position.y > gameObject.transform.position.y)
             {
-                if (printDebugInfo) Debug.Log(other.gameObject.name + " added");
-                objects.Add(new objEntry(other.gameObject));
+                if (printDebugInfo) Debug.Log(gameObject.name+" > "+other.gameObject.name + " added");
+                objects.Add(new objEntry(other.gameObject, rb));
+                rb.velocity = new Vector2(0,0);
             }
         }
     }
     void OnCollisionExit2D(Collision2D other)
     {
-        if (printDebugInfo) Debug.Log(other.gameObject.name + " collision exit");
+        if (printDebugInfo) Debug.Log(gameObject.name + " > "+other.gameObject.name + " collision exit");
         objects.RemoveAll(o => o.gameObject == other.gameObject);
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
--conveyor animation speed should change with speed variable.
+-Sometimes the player can land on the bottom side of a platform collision box for platforms with an effector
 
--Bouncy block doesn't push the player very far when he collides horizontally with it.
+-Bouncy block doesn't push the player very far when he collides horizontally with it. Maybe check if it's sideways and add additional force?
 
--Add other forms of movement to moving platforms
+-Add other forms of movement to moving platforms, and maybe a pause option
 
 -When pushing against wall:
 NullReferenceException: Object reference not set to an instance of an object
