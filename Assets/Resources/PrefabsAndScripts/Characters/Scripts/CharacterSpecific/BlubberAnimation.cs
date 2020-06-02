4b75007510936e44be5a3c4549ba736b0cd09dd8
jabackf <jabackf@yahoo.com>
2020-06-02 10:20:55 -0700

    Addded surprise and emoteIcons

--- Assets/Resources/PrefabsAndScripts/Characters/Scripts/CharacterSpecific/BlubberAnimation.cs
+++ Assets/Resources/PrefabsAndScripts/Characters/Scripts/CharacterSpecific/BlubberAnimation.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class BlubberAnimation : CharacterAnimation
 {
-	[Space]
+
+    /*List of character emotions
+     * 
+     *+ Normal
+     *+ Love
+     *+ Surprise (exclamation mark)
+     * Cry
+     * Sad
+     * Concern (single teardrop, anime style)
+     *+ Squint
+     * Excited (fast jumping up and down, closed eyes)
+     *+ Angry
+     * cuteEyes
+     *+ bugEyes
+     *+ smallEyes
+     * eyesClosed
+     * 
+     * Other:
+     * 
+     * facePlayer
+     * 
+     */
+
+
+    [Space]
     [Header("Blubber Emotions")]
-	
+
+    private Global global;
+
     multiDress eyes;
 
     bool blink = false;
@@ -14,18 +40,25 @@ public class BlubberAnimation : CharacterAnimation
     float blinkTimerMax = 8f;
     float blinkTimer = 0f;
     float blinkCloseTime = 0.25f;
-    string emotion = "Normal";
+    string currentEyes = "Normal";
+    string currentEmotion = "Normal";
 
+    //Character particles are used for various effects, like the hearts that come from the character's love emotion
     private GameObject particles;
-    private string particlesEmotion = "";
 
-    public string particlesLoveResource = "psHeartParticles";
-	
-	private Global global;
+    public GameObject particlesLovePrefab;
+
+
+    //Emote icons are icons that can appear near the character to display emotions such as an exclamation point or tear drop
+    private GameObject emoteIcon;
+
+    public GameObject eiExclamationPoint;
+
+
 
     void Start()
     {
-		global = GameObject.FindWithTag("global").GetComponent<Global>();
+        global = GameObject.FindWithTag("global").GetComponent<Global>();
         base.Start();
     }
 
@@ -33,19 +66,21 @@ public class BlubberAnimation : CharacterAnimation
     {
         blinkTimer = UnityEngine.Random.Range(blinkTimerMin, blinkTimerMax);
 
-        dressList.Add(new dress("eyesNormal", global.dirBlubberSprites+"Eyes", gameObject.transform, false));
-        dressList.Add(new dress("eyesAngry", global.dirBlubberSprites+"eyesAngry", gameObject.transform, false));
-        dressList.Add(new dress("eyesBlink", global.dirBlubberSprites+"eyesBlink", gameObject.transform, false));
-        dressList.Add(new dress("eyesClimb", global.dirBlubberSprites+"eyesClimb", gameObject.transform, false));
-        dressList.Add(new dress("eyesHalf", global.dirBlubberSprites+"eyesHalf", gameObject.transform, false));
-        dressList.Add(new dress("eyesLove", global.dirBlubberSprites+"eyesLove", gameObject.transform, false));
+        dressList.Add(new dress("eyesNormal", global.dirBlubberSprites + "Eyes", gameObject.transform, false));
+        dressList.Add(new dress("eyesAngry", global.dirBlubberSprites + "eyesAngry", gameObject.transform, false));
+        dressList.Add(new dress("eyesBlink", global.dirBlubberSprites + "eyesBlink", gameObject.transform, false));
+        dressList.Add(new dress("eyesClimb", global.dirBlubberSprites + "eyesClimb", gameObject.transform, false));
+        dressList.Add(new dress("eyesHalf", global.dirBlubberSprites + "eyesHalf", gameObject.transform, false));
+        dressList.Add(new dress("eyesLove", global.dirBlubberSprites + "eyesLove", gameObject.transform, false));
+        dressList.Add(new dress("eyesSmall", global.dirBlubberSprites + "eyesSmall", gameObject.transform, false));
+        dressList.Add(new dress("eyesBug", global.dirBlubberSprites + "eyesBug", gameObject.transform, false));
 
-        eyes = new multiDress(ref dressList, "eyesNormal", new string[] { "eyesNormal","eyesAngry","eyesBlink","eyesClimb", "eyesHalf", "eyesLove" });
+        eyes = new multiDress(ref dressList, "eyesNormal", new string[] { "eyesNormal","eyesAngry","eyesBlink","eyesClimb", "eyesHalf", "eyesLove", "eyesSmall", "eyesBug" });
 
     }
     public override void UpdateCharacter()
     {
-        switch(state)
+        switch (state)
         {
             case states.pushing:
                 eyes.changeState("eyesAngry");
@@ -55,7 +90,7 @@ public class BlubberAnimation : CharacterAnimation
                 break;
 
             default:
-                eyes.changeState("eyes"+emotion);
+                eyes.changeState("eyes" + currentEyes);
                 break;
         }
 
@@ -76,40 +111,77 @@ public class BlubberAnimation : CharacterAnimation
         }
     }
 
+    //Sets the particles to specified prefab. Pass null for no particles
+    public void setParticles(GameObject prefab)
+    {
+        if (particles != null) Destroy(particles);
+        if (prefab!=null)
+        {
+            particles = (GameObject)Instantiate(prefab);
+            particles.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.8f, gameObject.transform.position.z);
+        }
+    }
+
+    //Sets the emote icon to specified prefab. Pass null for no emote icon
+    public void setEmoteIcon(GameObject prefab)
+    {
+        if (emoteIcon != null) Destroy(emoteIcon);
+        if (prefab!=null)
+        {
+            emoteIcon = (GameObject)Instantiate(prefab);
+            emoteIcon.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+            emoteIcon.transform.parent = gameObject.transform;
+        }
+    }
+
+    //START EMOTION IMPLEMENTATION
+
     public void Angry()
     {
         eyes.changeState("eyesAngry");
-        emotion = "Angry";
-        setParticles("none");
+        currentEyes = "Angry";
+        currentEmotion = "Angry";
+        setParticles(null);
+        setEmoteIcon(null);
     }
     public void Normal()
     {
         eyes.changeState("eyesNormal");
-        emotion = "Normal";
-        setParticles("none");
+        currentEyes = "Normal";
+        currentEmotion = "Normal";
+        setParticles(null);
+        setEmoteIcon(null);
     }
     public void Love()
     {
-        eyes.changeState("eyesHalf");
-        emotion = "Love";
-        setParticles(particlesLoveResource);
+        eyes.changeState("eyesLove");
+        currentEyes = "Love";
+        currentEmotion = "Love";
+        setParticles(particlesLovePrefab);
+        setEmoteIcon(null);
     }
-
-    public void setParticles(string particleResource)
+    public void Squint()
     {
-        if (particleResource=="none")
-        {
-            if (particles != null) Destroy(particles);
-            particlesEmotion = "";
-        }
-        else if (particlesEmotion != emotion)
-        {
-            if (particles != null) Destroy(particles);
-            particlesEmotion = emotion;
-            particles = (GameObject)Instantiate(Resources.Load(global.dirParticleSystems+particleResource));
-            //particles.transform.parent = gameObject.transform;
-            particles.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.8f, gameObject.transform.position.z);
-            //particles.transform.localScale = new Vector3(1, 1, 1);
-        }
+        eyes.changeState("eyesHalf");
+        currentEyes = "Half";
+        currentEmotion = "Squint";
+        setEmoteIcon(null);
+    }
+    public void Surprise()
+    {
+        eyes.changeState("eyesBug");
+        currentEyes = "Bug";
+        currentEmotion = "Surprise";
+        setEmoteIcon(eiExclamationPoint);
+    }
+    public void smallEyes()
+    {
+        eyes.changeState("eyesSmall");
+        currentEyes = "Small";
+    }
+    public void bugEyes()
+    {
+        eyes.changeState("eyesBug");
+        currentEyes = "Bug";
     }
 }
---
+++ Assets/Resources/PrefabsAndScripts/GenericBehaviors/offsetPosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script simply applies an offset vector to the object
+
+public class offsetPosition : MonoBehaviour
+{
+    public Vector3 offset = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position += offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
